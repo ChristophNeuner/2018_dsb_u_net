9ace46a1b6ba063108a3a977eb7c452a2b9c005d
ChristophNeuner <christoph_neuner@web.de>
2018-04-29 18:12:25 +0000

    reorganized code

---
+++ model.py
@@ -0,0 +1,104 @@
+import os
+import utils
+import tensorflow as tf
+import numpy as np
+from datetime import datetime
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import cv2
+from skimage.transform import resize
+
+# Build U-Net model
+def build_model(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):   
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = Lambda(lambda x: x / 255) (inputs)
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (s)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[utils.dice_coef])
+    model.summary()
+    return model
+
+    
+#Fit model
+def fit_model(model, MODEL_DIR, X_train, Y_train):
+    earlystopper = EarlyStopping(patience=5, verbose=1)
+    currentModelDir = os.path.join(MODEL_DIR, datetime.now().strftime("%Y-%m-%d %H:%M:%S"),)
+    if not os.path.exists(currentModelDir):
+        os.makedirs(currentModelDir)
+    filepath = os.path.join(currentModelDir, 'epoch{epoch:04d}-val_loss{val_loss:.2f}.h5')
+    checkpointer = ModelCheckpoint(filepath, verbose=1, save_best_only=True)
+    results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=8, epochs=30, 
+                        callbacks=[earlystopper, checkpointer])
+    
+    
+#Make predictions
+#Predict on train, val and test
+def make_predictions(model_path, X_train, X_test, sizes_test):
+    model = load_model(model_path, custom_objects={'dice_coef': utils.dice_coef})
+    preds_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+    preds_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+    preds_test = model.predict(X_test, verbose=1)
+
+    # Threshold predictions
+    preds_train_t = (preds_train > 0.5).astype(np.uint8)
+    preds_val_t = (preds_val > 0.5).astype(np.uint8)
+    preds_test_t = (preds_test > 0.5).astype(np.uint8)
+
+    # Create list of upsampled test masks
+    preds_test_upsampled = []
+    for i in range(len(preds_test)):
+        #preds_test_upsampled.append(resize(np.squeeze(preds_test[i]), 
+                                           #(sizes_test[i][0], sizes_test[i][1]), 
+                                           #mode='constant', preserve_range=True))
+        preds_test_upsampled.append(cv2.resize(np.squeeze(preds_test[i]),
+                                                (sizes_test[i][0], sizes_test[i][1]),
+                                                interpolation=cv2.INTER_AREA))
+    return preds_train_t, preds_val_t, preds_test_upsampled
---
+++ utils.py
@@ -0,0 +1,120 @@
+import os
+import numpy as np
+import sys
+import cv2
+from tqdm import tqdm
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+from keras import backend as K
+
+smooth = 1.
+
+def get_data(TRAIN_PATH, TEST_PATH, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS):
+    # Get train and test IDs
+    train_ids = next(os.walk(TRAIN_PATH))[1]
+    test_ids = next(os.walk(TEST_PATH))[1]
+
+    # Get and resize train images and masks
+    X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+    print('Getting and resizing train images and masks ... ')
+    sys.stdout.flush()
+    for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+        path = TRAIN_PATH + id_
+        try:
+            img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+        except:
+            print(id_)
+            continue
+        ###resize with skimage.transform
+        #img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        ###resize with cv2
+        img = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_AREA)
+        ###scale image channels
+        img = scale_img_channels(img, IMG_CHANNELS)
+        X_train[n] = img
+        mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+        for mask_file in next(os.walk(path + '/masks/'))[2]:
+            mask_ = imread(path + '/masks/' + mask_file)
+            mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant', 
+                                          preserve_range=True), axis=-1)
+            mask = np.maximum(mask, mask_)
+        Y_train[n] = mask
+
+    # Get and resize test images
+    X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+    sizes_test = []
+    print('Getting and resizing test images ... ')
+    sys.stdout.flush()
+    for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+        path = TEST_PATH + id_
+        try:
+            img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+        except:
+            print(id_)
+        sizes_test.append([img.shape[0], img.shape[1]])
+        ###resize with skimage.transform
+        #img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+        ###resize with cv2
+        img = cv2.resize(img, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_AREA)
+        ###scale image channels
+        img = scale_img_channels(img, IMG_CHANNELS)
+        X_test[n] = img
+        
+    print('Done!')
+    return train_ids, test_ids, X_train, Y_train, X_test, sizes_test
+
+
+
+def scale_img_channels(an_img, img_channels):
+    for i in range(img_channels):
+        channel = an_img[:,:,i]
+        channel = channel - channel.min()
+        channelmax = channel.max()
+        if channelmax > 0:
+            factor = 255/channelmax
+            channel = (channel * factor).astype(int)
+        an_img[:,:,i] = channel
+        return an_img
+    
+
+# Define IoU metric
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred &gt > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2, y_true)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+# Metric function
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+# Loss funtion
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+# Run-length encoding stolen from https://www.kaggle.com/rakhlin/fast-run-length-encoding-python
+def rle_encoding(x):
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b>prev+1): run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
+
+def prob_to_rles(x, cutoff=0.5):
+    lab_img = label(x > cutoff)
+    for i in range(1, lab_img.max() + 1):
+        yield rle_encoding(lab_img == i)
